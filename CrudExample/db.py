8dc1658b14be6d7c5571e9e136f3a27adeee6efb
KIKIMORA <sasha.kovalov2008@gmail.com>
2025-06-06 17:46:59 +0300

    change DATABASE_URL

--- CrudExample/db.py
+++ CrudExample/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/postgres"
+DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@localhost/mydb"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
